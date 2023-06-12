a5b01de0c5a76ff11f9a6da57f530be49531ed91
Matt Chan <matthew.chan@icloud.com>
2023-06-12 04:41:32 +0100

    Removing absent API code

--- src/api/products/[id].ts
+++ src/api/products/[id].ts
@@ -1,15 +1,15 @@
-import { NextApiRequest, NextApiResponse } from 'next';
-import { getProductById } from '@/pages/api-helpers/products';
+// import { NextApiRequest, NextApiResponse } from 'next';
+// import { getProductById } from '@/pages/api-helpers/products';
 
-export default async function handler(
-  req: NextApiRequest,
-  res: NextApiResponse
-) {
-  const { id } = req.query;
-  const product = await getProductById(Number(id));
-  if (product) {
-    res.status(200).json(product);
-  } else {
-    res.status(404).json({ message: 'Product not found' });
-  }
-}
+// export default async function handler(
+//   req: NextApiRequest,
+//   res: NextApiResponse
+// ) {
+//   const { id } = req.query;
+//   const product = await getProductById(Number(id));
+//   if (product) {
+//     res.status(200).json(product);
+//   } else {
+//     res.status(404).json({ message: 'Product not found' });
+//   }
+// }
